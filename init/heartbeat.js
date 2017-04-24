315fc33e750168b5a472371a3a9e521ff09f4807
Rob L <viewpl@gmail.com>
2017-04-25 01:10:08 +1000

    Fix two small issues

--- init/heartbeat.js
+++ init/heartbeat.js
@@ -3,14 +3,13 @@ const logger = log4js.getLogger('heartbeat');
 const config = appRequire('services/config').all();
 
 const checkAccount = appRequire('plugins/account/checkAccount');
-const shadowsocksServices = undefined;
+let shadowsocksServices = undefined;
 if (config.type === 's') {
   shadowsocksServices = appRequire('services/shadowsocks');
 }
 
 const beatForever = () => {
   checkAccount.handleInterval();
-  console.log(JSON.stringify(config, null, 2));
   if (shadowsocksServices !== undefined) {
     shadowsocksServices.handleInterval();
   }
